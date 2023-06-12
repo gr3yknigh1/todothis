73df7e2dd95fc8ccb739f490413917633f9b69cb
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-06-12 20:36:40 +0300

    Log initial info

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,11 +1,23 @@
 /*
  * Main server of `todothis`
  * */
+#include <cstdint>
 #include <cstdlib>
+
+#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+    auto logger = spdlog::default_logger();
+    logger->set_level(spdlog::level::trace);
+
+    logger->debug("Running with arguments:");
+    for (int i = 0; i < argc; ++i)
+    {
+        logger->debug("> [{}]: {}", i, argv[i]);
+    }
+    logger->info("Starting server...");
+
+    logger->info("Stopping server...");
     return EXIT_SUCCESS;
 }
